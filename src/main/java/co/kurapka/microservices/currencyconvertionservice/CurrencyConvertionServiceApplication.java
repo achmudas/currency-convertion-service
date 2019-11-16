3f658f1d943d9e01000f9f0a048b6b475d6a42eb
Remigijus <achmudass@gmail.com>
2019-11-16 18:54:03 +0200

    Using Eureka for discovery

--- src/main/java/co/kurapka/microservices/currencyconvertionservice/CurrencyConvertionServiceApplication.java
+++ src/main/java/co/kurapka/microservices/currencyconvertionservice/CurrencyConvertionServiceApplication.java
@@ -2,8 +2,10 @@ package co.kurapka.microservices.currencyconvertionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients("co.kurapka.microservices.currencyconvertionservice")
 public class CurrencyConvertionServiceApplication {
--- src/test/java/co/kurapka/microservices/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java
+++ src/test/java/co/kurapka/microservices/currencyconvertionservice/CurrencyConvertionServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package co.kurapka.microservices.currencyconvertionservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
