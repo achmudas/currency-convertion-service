a2d51f4e5bcc11363eeeab972c6d5c3ee13849a9
Remigijus <achmudass@gmail.com>
2019-11-17 16:54:40 +0200

    Introducing API gateway

--- src/main/java/co/kurapka/microservices/currencyconvertionservice/CurrencyExchangeProxy.java
+++ src/main/java/co/kurapka/microservices/currencyconvertionservice/CurrencyExchangeProxy.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
+@RibbonClient(name = "netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConvertionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
